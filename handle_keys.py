75a12dfbb4c4aca36a52d45d1c734d46ef449161
sean-huang-10 <seanwc564@gmail.com>
2024-08-09 19:19:10 +0800

    line

--- handle_keys.py
+++ handle_keys.py
@@ -13,6 +13,9 @@ def get_secret_and_token():
     if channel_access_token is None:
         print('Specify LINEBOT_ACCESS_TOKEN as environment variable.')
         sys.exit(1)
+    if openai_api_key is None:
+        print('Specify OPEN_API_KEY as environment variable.')
+        sys.exit(1)    
     return {
         'LINEBOT_SECRET_KEY':channel_access_token,
         'LINEBOT_ACCESS_TOKEN':channel_access_token
